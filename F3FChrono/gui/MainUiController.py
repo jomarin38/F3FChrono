18ec92bedc8645d393b5e85f51cdd1f3d75028da
sdaviet <sylvain.daviet@gmail.com>
2019-12-15 18:27:16 +0100

    gui chrono display method change with QtimerEvent gui display in fullscreen mode

--- F3FChrono/gui/MainUiController.py
+++ F3FChrono/gui/MainUiController.py
@@ -10,10 +10,9 @@ from F3FChrono.data.dao.EventDAO import EventDAO
 
 class MainUiCtrl (QtWidgets.QMainWindow):
 
-    def __init__(self,dao,chrono):
+    def __init__(self,event,chrono):
         super().__init__()
-        self.dao = dao
-        self.event = None
+        self.event = event
         self.chrono = chrono
         self.initUI()
         self.base_test=0
@@ -25,7 +24,7 @@ class MainUiCtrl (QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
 
         self.ui.setupUi(self.MainWindow)
-        #self.MainWindow.showFullScreen()
+        self.MainWindow.showFullScreen()
         self.controllers = collections.OrderedDict()
 
         self.controllers['config'] = WConfigCtrl("panel Config", self.ui.centralwidget)
@@ -37,7 +36,6 @@ class MainUiCtrl (QtWidgets.QMainWindow):
                 self.ui.verticalLayout.addWidget(x)
 
         self.controllers['config'].btn_next_sig.connect(self.start)
-        self.controllers['config'].contest_sig.connect(self.contest_changed)
         self.controllers['round'].btn_next_sig.connect(self.next_action)
         self.controllers['round'].btn_home_sig.connect(self.show_config)
         self.controllers['round'].btn_refly_sig.connect(self.refly)
@@ -46,9 +44,6 @@ class MainUiCtrl (QtWidgets.QMainWindow):
 
         self.show_config()
         self.MainWindow.show()
-        self.controllers['config'].set_contest(self.dao.get_list())
-        self.controllers['wind'].set_data(0, 0)
-
 
     def show_config(self):
         self.controllers['round'].hide()
@@ -79,9 +74,7 @@ class MainUiCtrl (QtWidgets.QMainWindow):
         self.event.max_wind_dir_dev=self.controllers['config'].wind_orientation
         self.event.min_allowed_wind_speed=self.controllers['config'].wind_speed_min
         self.event.max_allowed_wind_speed=self.controllers['config'].wind_speed_max
-
         self.chrono.reset()
-        self.controllers['round'].wPilotCtrl.set_data(self.event.get_current_round().get_current_competitor())
         self.controllers['round'].wChronoCtrl.set_status(self.chrono.get_status())
         self.show_chrono()
         self.controllers['round'].wChronoCtrl.reset_ui()
@@ -99,7 +92,7 @@ class MainUiCtrl (QtWidgets.QMainWindow):
                 if (self.chrono.getLapCount() == 10):
                     self.controllers['round'].wChronoCtrl.stoptime()
                     self.chrono.next_status()
-                #print("chronolapcount : "+str(self.chrono.getLapCount()))
+                print("chronolapcount : "+str(self.chrono.getLapCount()))
                 self.chrono.declareBase(self.base_test)
                 self.base_test=~self.base_test
                 self.controllers['round'].wChronoCtrl.set_laptime(self.chrono.getLastLapTime())
@@ -127,23 +120,27 @@ class MainUiCtrl (QtWidgets.QMainWindow):
         "TODO Insert event class null flight function"
         print("null flight event")
 
-    def contest_changed(self):
-        self.event=self.dao.get(self.controllers['config'].view.ContestList.currentIndex(),
-                                fetch_competitors=True, fetch_rounds=True, fetch_runs=True)
-
-        self.controllers['config'].set_data(self.event.min_allowed_wind_speed,
+    def set_initial_data(self):
+        print("initial_data method")
+        self.controllers['round'].wPilotCtrl.set_data(self.event.get_current_round().get_current_competitor())
+        self.controllers['wind'].set_data(0, 0)
+        self.controllers['config'].set_data(self.event.location,
+                                            self.event.min_allowed_wind_speed,
                                             self.event.max_allowed_wind_speed,
                                             self.event.max_wind_dir_dev,
                                             self.event.max_interruption_time)
 
 def main ():
+    contest_id = 1
 
     dao = EventDAO()
+    event = dao.get(contest_id, fetch_competitors=True, fetch_rounds=True, fetch_runs=True)
     chrono = Chrono()
 
     app = QtWidgets.QApplication(sys.argv)
-    ui=MainUiCtrl(dao, chrono)
+    ui=MainUiCtrl(event, chrono)
 
+    ui.set_initial_data()
 
     try:
         # writer.setupDecoder()
