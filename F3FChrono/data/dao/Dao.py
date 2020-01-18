71a16364aa4f65a532baece019fe339ab3e7e8fe
Joel Marin <joel.marin@neuf.fr>
2020-01-18 22:08:59 +0100

    Reverting bad change

--- F3FChrono/data/dao/Dao.py
+++ F3FChrono/data/dao/Dao.py
@@ -3,7 +3,7 @@ import pymysql
 
 class Dao:
 
-    _db = pymysql.connect("localhost", "f3f_ctrl", "F3FCtrl", "f3f_chrono_sdaviet")
+    _db = pymysql.connect("localhost", "f3f_ctrl", "F3FCtrl", "f3f_chrono")
 
     def __init__(self):
         self._cursor = Dao._db.cursor()
