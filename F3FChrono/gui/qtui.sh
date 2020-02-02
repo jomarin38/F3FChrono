1d219a72e3f984a5295782ee91d664ab07e48cbe
sdaviet <sylvain.daviet@gmail.com>
2020-02-02 22:41:12 +0100

    Adding wind speed & orientation stats during a run in chrono class Adding Vocal & sound
    adding a new Sound class using PyQt sound module.
    A Speech module is available on PySide2 but not in PyQt5
    
    GPIO class created
    diffents little evolutions.

--- F3FChrono/gui/qtui.sh
+++ F3FChrono/gui/qtui.sh
@@ -7,3 +7,5 @@ python3 -m PyQt5.uic.pyuic -x WConfig.ui -o WConfig_ui.py
 python3 -m PyQt5.uic.pyuic -x WChrono.ui -o WChrono_ui.py
 python3 -m PyQt5.uic.pyuic -x WChronoBtn.ui -o WChronoBtn_ui.py
 python3 -m PyQt5.uic.pyuic -x simulate_base.ui -o simulate_base_ui.py
+python3 -m PyQt5.uic.pyuic -x WSettings.ui -o WSettings_ui.py
+python3 -m PyQt5.uic.pyuic -x WSettingsAdvanced.ui -o WSettingsAdvanced_ui.py
