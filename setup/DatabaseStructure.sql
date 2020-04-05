48d36f60e8dbc36d8d77795d52f3605a1d22ab7a
sdaviet <sylvain.daviet@gmail.com>
2020-04-05 09:26:03 +0200

    Update sql structure

--- setup/DatabaseStructure.sql
+++ setup/DatabaseStructure.sql
@@ -27,6 +27,7 @@ CREATE TABLE `chrono` (
   `run_time` double DEFAULT NULL,
   `min_wind_speed` double DEFAULT NULL,
   `max_wind_speed` double DEFAULT NULL,
+  'mean_wind_speed' double DEFAULT NULL,
   `wind_direction` double DEFAULT NULL,
   `start_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `end_date` timestamp NULL DEFAULT NULL,
@@ -80,6 +81,8 @@ CREATE TABLE `event` (
   `max_allowed_wind_speed` double DEFAULT NULL,
   `max_wind_dir_dev` double DEFAULT NULL,
   `max_interruption_time` double DEFAULT NULL,
+  `bib_start` int(11) DEFAULT 0,
+  `dayduration` int(11) DEFAULT 1,
   `f3x_vault_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`event_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
