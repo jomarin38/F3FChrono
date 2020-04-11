610de94bb73c295a8d1a225ab62fd6ef226ceb96
sdaviet <sylvain.daviet@gmail.com>
2020-04-11 11:30:22 +0200

    fix issue remove first lap at starting

--- F3FChrono/chrono/Chrono.py
+++ F3FChrono/chrono/Chrono.py
@@ -288,6 +288,10 @@ class ChronoArduino(ChronoHard, QTimer):
         self.chronoLap.clear()
         self.penalty = 0.0
         self.reset_wind()
+        self.currentlap=0
+        self.oldlap=0
+        self.oldstatus=0
+        
 
     def timerEvent(self):
         if self.arduino is not None and not ('fake_rpi' in sys.modules) :
