a767ba5f6f7eefec5a1d177b6631c9437f1fbfce
Joel Marin <joel.marin@neuf.fr>
2020-04-20 21:24:08 +0200

    Small modif to be able to run on Linux PC

--- F3FChrono/chrono/rs232_arduino.py
+++ F3FChrono/chrono/rs232_arduino.py
@@ -11,7 +11,7 @@ class rs232_arduino (threading.Thread):
     def __init__(self, voltageCoef, rebundTimeBtn, buzzerTime, status_changed, run_started, lap_finished,
                  run_finished, wait_alt_sig, accu_sig):
         super().__init__()
-        self.bus=serial.Serial(port='/dev/ttyS0', baudrate = 57600, parity=serial.PARITY_NONE,
+        self.bus=serial.Serial(port=rs232_arduino.get_serial_port(), baudrate = 57600, parity=serial.PARITY_NONE,
                                  stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=0.2)
         print(self.bus)
 
@@ -32,6 +32,29 @@ class rs232_arduino (threading.Thread):
         self.event.daemon = True
         self.event.start()
         self.inRun=False
+
+    @staticmethod
+    def get_serial_port():
+        rev_file = '/sys/firmware/devicetree/base/model'
+        info = {'pi': '', 'model': '', 'rev': ''}
+        raspi = model = revision = ''
+        try:
+            fd = os.open(rev_file, os.O_RDONLY)
+            line = os.read(fd, 256)
+            os.close(fd)
+            print(line)
+            m = re.split(r'\s', line.decode('utf-8'))
+            if m:
+                info['pi'] = m[3]
+                info['model'] = m[5]
+            return info
+        except:
+            pass
+
+        if info['pi'] == '':
+            return '/dev/ttyUSB0'
+        else:
+            return '/dev/ttyS0'
 
     def receive(self):
         while not self.terminated:
