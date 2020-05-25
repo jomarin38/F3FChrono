e124b852015e460a63912464661bbc9a5af383c1
Joel Marin <joel.marin@neuf.fr>
2020-05-25 21:53:59 +0200

    Conditional fake_rpi requirement

---
+++ F3FChrono/Utils.py
@@ -0,0 +1,24 @@
+
+
+def get_raspi_revision():
+    rev_file = '/sys/firmware/devicetree/base/model'
+    info = { 'pi': '', 'model': '', 'rev': ''}
+    raspi = model = revision = ''
+    try:
+        fd = os.open(rev_file, os.O_RDONLY)
+        line = os.read(fd,256)
+        os.close(fd)
+        print (line)
+        m=re.split(r'\s', line.decode('utf-8'))
+        if m:
+            info['pi'] = m[3]
+            info['model'] = m[5]
+        return info
+    except:
+        pass
+    return info
+
+
+def is_running_on_pi():
+    pi = get_raspi_revision()
+    return pi['pi'] != ''
--- runchrono.py
+++ runchrono.py
@@ -6,28 +6,10 @@ import time
 
 from argparse import ArgumentParser
 from PyQt5 import QtWidgets, QtCore
+from F3FChrono.Utils import is_running_on_pi
 import os.path
 
-def get_raspi_revision():
-    rev_file = '/sys/firmware/devicetree/base/model'
-    info = { 'pi': '', 'model': '', 'rev': ''}
-    raspi = model = revision = ''
-    try:
-        fd = os.open(rev_file, os.O_RDONLY)
-        line = os.read(fd,256)
-        os.close(fd)
-        print (line)
-        m=re.split(r'\s', line.decode('utf-8'))
-        if m:
-            info['pi'] = m[3]
-            info['model'] = m[5]
-        return info
-    except:
-        pass
-    return info
-
-pi=get_raspi_revision()
-if pi['pi'] == '':
+if not is_running_on_pi():
     # Replace libraries by fake ones
     import sys
     import fake_rpi
