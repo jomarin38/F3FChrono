f0a8e09fb7c6bc95f6d68140f48da83e7fbcb242
Joel Marin <joel.marin@neuf.fr>
2020-06-12 22:58:29 +0200

    First wireless button test

---
+++ arduino/WirelessNextButton/WirelessNextButton.ino
@@ -0,0 +1,54 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+#ifndef STASSID
+#define STASSID "F3FCtrl"
+#define STAPSK  "F3FPassword"
+#endif
+
+
+// buffers for receiving and sending data
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE + 1]; //buffer to hold incoming packet,
+char  ReplyBuffer[] = "Event\r\n";       // a string to send back
+
+char remoteIP[] = "255.255.255.255";
+int remotePort = 4445;
+
+const int buttonPin = 2;
+int buttonState = 0;
+
+WiFiUDP Udp;
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(STASSID, STAPSK);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(500);
+  }
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
+
+  pinMode(0, OUTPUT);
+  digitalWrite(0, LOW);
+
+  pinMode(buttonPin, INPUT);
+  
+}
+
+void loop() {
+
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState == LOW) {
+
+    Udp.beginPacket(remoteIP, remotePort);
+    Udp.write(ReplyBuffer);
+    Udp.endPacket();
+    delay(500);
+
+  }
+
+}
+
