0706c833f214049e095c69fca484ae126244a11e
Joel Marin <joel.marin@neuf.fr>
2020-08-21 22:21:06 +0200

    Adding signal pressed, clicked and longPressed

--- arduino/WirelessNextButton/WirelessNextButton.ino
+++ arduino/WirelessNextButton/WirelessNextButton.ino
@@ -18,6 +18,15 @@ int buttonState = 0;
 int count=0;
 WiFiUDP Udp;
 
+bool buttonReleased;
+bool buttonPressed;
+
+enum Event {
+  pressed,
+  clicked,
+  longPressed
+};
+
 void setup() {
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
@@ -33,7 +42,27 @@ void setup() {
   digitalWrite(0, LOW);
 
   pinMode(buttonPin, INPUT);
+
+  buttonReleased = true;
+  buttonPressed = false;
   
+}
+
+void sendMessage(Event e) {
+  strcpy (message_wBtn, "wBtn ");
+  if (e==pressed) {
+    strcat (message_wBtn, "-1");
+  }
+  else if (e==clicked) {
+    strcat (message_wBtn, "1");
+  }
+  else if (e==longPressed) {
+    strcat (message_wBtn, "0");
+  }
+  strcat (message_wBtn, "\r\n");
+  Udp.beginPacket(remoteIP, remotePort);
+  Udp.write(message_wBtn);
+  Udp.endPacket();
 }
 
 void loop() {
@@ -41,18 +70,27 @@ void loop() {
   buttonState = digitalRead(buttonPin);
 
   if (buttonState == LOW) {
-    delay(100);
-    count++;
-  }else{
-    if (count>0){
-      strcpy (message_wBtn, "wBtn ");
-      strcat (message_wBtn, ((count>=10)?"0":"1"));
-      strcat (message_wBtn, "\r\n");
-      Udp.beginPacket(remoteIP, remotePort);
-      Udp.write(message_wBtn);
-      Udp.endPacket();
-      count=0;   
-     }
+    if (buttonReleased && count==0) {
+      sendMessage(pressed);
+      buttonReleased=false;
+      buttonPressed=true;
+      count++;
+    }
+    else if (count>=100) {
+      sendMessage(longPressed);
+      count = 0 ;
+      buttonPressed=false;
+    }
+    else if (buttonPressed) {
+      count++;
+    }
+  }else if (buttonState == HIGH) {
+    if (buttonPressed && count>0 && count<=100) {
+      sendMessage(clicked);
+    }
+    count = 0;
+    buttonReleased = true;
+    buttonPressed = false;
   }
-  delay(5);
+  delay(10);
 }
