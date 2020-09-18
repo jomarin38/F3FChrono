79524040a335f3ce2953ce61b5fca34fb7b1a226
Joel Marin <joel.marin@neuf.fr>
2020-09-18 22:08:26 +0200

    Fixing database access concurrent access

--- F3FChrono/data/dao/Dao.py
+++ F3FChrono/data/dao/Dao.py
@@ -1,27 +1,39 @@
 import pymysql
+import threading
 
 
 class Dao:
 
     _db = pymysql.connect("localhost", "f3f_ctrl", "F3FCtrl", "f3f_chrono")
+    _lock = threading.Lock()
 
     def __init__(self):
+        Dao._lock.acquire()
         self._cursor = Dao._db.cursor()
+        Dao._lock.release()
 
     def _execute_query(self, sql, *args):
+        Dao._lock.acquire()
         self._cursor.execute(sql, args)
         result = self._cursor.fetchall()
         Dao._db.commit()
+        Dao._lock.release()
         return result
 
     def _execute_insert(self, sql, *args):
+        Dao._lock.acquire()
         self._cursor.execute(sql, args)
+        Dao._lock.release()
         Dao._db.commit()
 
     def _execute_update(self, sql, *args):
+        Dao._lock.acquire()
         self._cursor.execute(sql, args)
         Dao._db.commit()
+        Dao._lock.release()
 
     def _execute_delete(self, sql, *args):
+        Dao._lock.acquire()
         self._cursor.execute(sql, args)
         Dao._db.commit()
+        Dao._lock.release()
