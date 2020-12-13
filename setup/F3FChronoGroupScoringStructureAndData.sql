279513194e6004e30962474e32a8cc628f1a292d
Joel Marin <joel.marin@neuf.fr>
2020-12-13 19:21:25 +0100

    Include create schema

--- setup/F3FChronoGroupScoringStructureAndData.sql
+++ setup/F3FChronoGroupScoringStructureAndData.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `f3f_chrono` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `f3f_chrono`;
 -- MySQL dump 10.17  Distrib 10.3.25-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: 127.0.0.1    Database: f3f_chrono
@@ -257,4 +259,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-13 19:14:33
+-- Dump completed on 2020-12-13 19:18:23
