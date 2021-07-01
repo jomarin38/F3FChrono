8cb03e56f2c2c64c661d01590c3b51de631dc0a9
Joel Marin <joel.marin@neuf.fr>
2021-07-01 22:14:49 +0200

    sending flight order to F3XVault seems OK

--- F3FChrono/data/RoundGroup.py
+++ F3FChrono/data/RoundGroup.py
@@ -1,4 +1,5 @@
 import os
+from collections import OrderedDict
 
 from F3FChrono.data.dao.RoundDAO import RoundDAO
 from F3FChrono.data.dao.RunDAO import RunDAO
@@ -53,12 +54,13 @@ class RoundGroup:
             return []
 
     #Used in F3XVault export
-    def get_valid_flight_flight_order(self, competitor):
+    def get_valid_flight_order(self, competitor):
         if competitor.bib_number in self._flight_order:
-            for run in self.runs[competitor]:
-                if run.valid:
-                    return run
-        return 0
+            #remove duplicates, keep last flight for each competitor
+            cleaned_flight_order = list(reversed(OrderedDict.fromkeys(reversed(self._flight_order))))
+            index = cleaned_flight_order.index(competitor.bib_number)
+            return index + 1
+        return None
 
     def next_pilot(self, insert_database=False, visited_competitors=[]):
         if self._current_competitor_index < len(self._flight_order) - 1:
