4a9c59daa02af115baf3cda30d218f26ef63ac52
sdaviet <sylvain.daviet@gmail.com>
2022-03-21 21:32:58 +0100

    debug

--- F3FChrono/chrono/rs232_arduino.py
+++ F3FChrono/chrono/rs232_arduino.py
@@ -58,7 +58,7 @@ class rs232_arduino (QObject):
         self.inRun = False
         self.kill_aduino()
         self.training = False
-        self.__debug = False
+        self.__debug = True
         self.finaltime = 0.0
 
     @staticmethod
