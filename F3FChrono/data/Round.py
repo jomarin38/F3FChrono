e9353338f15cd42673558d9a1655dfd9e639aa5a
sdaviet <sylvain.daviet@gmail.com>
2022-04-21 22:19:53 +0200

    pull request comments

--- F3FChrono/data/Round.py
+++ F3FChrono/data/Round.py
@@ -457,8 +457,6 @@ class Round:
         groups = []
         result_dict['weather'] = {}
         if len(self.groups)>0:
-            #result_dict['weather']['wind'] = self.groups[-1].runs[list(self.groups[-1].runs.keys())[-1]][0].chrono.mean_wind_speed
-            #result_dict['weather']['orient'] = self.groups[-1].runs[list(self.groups[-1].runs.keys())[-1]][0].chrono.wind_direction
             result_dict['weather']['wind'] = self.groups[-1].runs[next(reversed(self.groups[-1].runs))][
                 0].chrono.mean_wind_speed
             result_dict['weather']['orient'] = self.groups[-1].runs[next(reversed(self.groups[-1].runs))][
