2b75ca817c5f3ff9080f1e5c0412aae3e5df01df
sdaviet <sylvain.daviet@gmail.com>
2024-03-06 23:02:24 +0100

    add debug function add display first message

--- arduino/DCDisplayWemos/DCDisplayWemos.ino
+++ arduino/DCDisplayWemos/DCDisplayWemos.ino
@@ -1,5 +1,5 @@
  /** 
- # This file is part of the F3FDCDisplay distribution (https://github.com/ADU).
+ # This file is part of the F3FDCDisplay distribution (https://github.com/jomarin38/F3FChrono).
  # Copyright (c) 2024 Sylvain DAVIET, Joel MARIN.
  # 
  # This program is free software: you can redistribute it and/or modify  
@@ -15,14 +15,42 @@
  # along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+//#define _DEBUG_
+#define DEBUG_LN true
+#define DEBUG_NOLN false
+#define DEBUG_START true
+#define DEBUG_NOSTART false
 
+#ifdef _DEBUG_
+  static void DebugStr(bool debug, bool ln, const char *str)
+  {
+    if (debug){
+      Serial.print("DEBUG - ");
+    }
+    if (ln){
+      Serial.println(str);  
+    }else{
+      Serial.print(str);  
+    }
+  }
+#else
+  static void DebugStr(bool debug, bool ln, const char *str)
+  {
+    str;
+  }
+
+#endif
+
+char tmpStr[100]="";
 
 
 void setup() {
-  Serial.begin(115200);
-  TCPClient_StartWifi();
+  Serial.begin(57600);
+  delay(500);
+  display_Start();
   BP_Start();
   Analog_Start();
+  TCPClient_StartWifi();
 }
 
 void loop() {
--- arduino/DCDisplayWemos/analog.ino
+++ arduino/DCDisplayWemos/analog.ino
@@ -1,5 +1,5 @@
  /** 
- # This file is part of the F3FDCDisplay distribution (https://github.com/ADU).
+ # This file is part of the F3FDCDisplay distribution (https://github.com/jomarin38/F3FChrono).
  # Copyright (c) 2024 Sylvain DAVIET, Joel MARIN.
  # 
  # This program is free software: you can redistribute it and/or modify  
@@ -15,12 +15,13 @@
  # along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define ANALOG_TIMEOUT 30000
+#define ANALOG_TIMEOUT 300000
 unsigned long analogTimeOut;
 int analogValue;
 float voltage;
 
 void Analog_Start(void){
+  DebugStr(DEBUG_START, DEBUG_LN, "Analog Module Start");
   analogTimeOut = 0;
 }
 
@@ -28,11 +29,8 @@ void Analog_Read(void){
   if ((millis()-analogTimeOut)>ANALOG_TIMEOUT){
     analogValue = analogRead(A0);
     voltage = analogValue*15/1024;
-    Serial.print("analog value : ");
-    Serial.print(analogValue);
-    Serial.print(", ");
-    Serial.print(voltage);
-    Serial.println("V");
+    sprintf(tmpStr, "analog value:%i, %.1fV", analogValue, voltage);
+    DebugStr(DEBUG_START, DEBUG_LN, tmpStr);
     TcpClient_SendAnalog(voltage);
 
     analogTimeOut = millis();
