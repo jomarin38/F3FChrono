a9ebb2569569751b4888c2bb9963efaeda954728
sdaviet <sylvain.daviet@gmail.com>
2024-07-12 11:11:43 +0200

    Picametre timing display integration with the same hardware of F3FDCDisplay and 2*16 bits expander GPIO MCP23017 on I2C. First tested version

---
+++ arduino/PicametreWemos/PicametreWemos.ino
@@ -0,0 +1,65 @@
+ /** 
+ # This file is part of the F3FPicametre distribution (https://github.com/jomarin38/F3FChrono).
+ # Copyright (c) 2024 Sylvain DAVIET, Joel MARIN.
+ # 
+ # This program is free software: you can redistribute it and/or modify  
+ # it under the terms of the GNU General Public License as published by  
+ # the Free Software Foundation, version 3.
+ #
+ # This program is distributed in the hope that it will be useful, but 
+ # WITHOUT ANY WARRANTY; without even the implied warranty of 
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ # General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License 
+ # along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#define VERSION "0.1"
+//#define _DEBUG_
+#define DEBUG_LN true
+#define DEBUG_NOLN false
+#define DEBUG_START true
+#define DEBUG_NOSTART false
+
+#ifdef _DEBUG_
+  static void DebugStr(bool debug, bool ln, const char *str)
+  {
+    if (debug){
+      Serial.print("DEBUG - ");
+    }
+    if (ln){
+      Serial.println(str);  
+    }else{
+      Serial.print(str);  
+    }
+  }
+#else
+  static void DebugStr(bool debug, bool ln, const char *str)
+  {
+    str;
+  }
+
+#endif
+
+char tmpStr[100]="";
+
+
+void setup() {
+  delay(100);
+  serial_setup();
+  //BP_Start();
+  delay(2000);
+
+  display_Start();
+  Analog_Start();
+  serial_run();
+  TCPClient_StartWifi();
+}
+
+void loop() {
+  TCPClient_Run();
+  //BP_CheckChanged();
+  Analog_Read();
+  serial_run();
+  delay(50);
+}
