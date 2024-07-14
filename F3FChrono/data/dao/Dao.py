aac97d8838b9198293a0186eb2a59c8cc6371b30
Joel Marin <joel.marin@neuf.fr>
2024-07-14 21:51:36 +0200

    Try to fix error on pi

--- F3FChrono/data/dao/Dao.py
+++ F3FChrono/data/dao/Dao.py
@@ -22,16 +22,14 @@ from F3FChrono.chrono import ConfigReader
 
 
 class Dao:
-
+    _db = pymysql.connect(host=os.getenv('MYSQL_IP', 'localhost'),
+                              user="f3f_ctrl",
+                              password="F3FCtrl",
+                              db="f3f_chrono")
     _lock = threading.Lock()
 
     def __init__(self):
         Dao._lock.acquire()
-        host = os.getenv('MYSQL_IP', 'localhost')
-        Dao._db = pymysql.connect(host=host,
-                          user="f3f_ctrl",
-                          password="F3FCtrl",
-                          db="f3f_chrono")
         self._cursor = Dao._db.cursor()
         Dao._lock.release()
 
